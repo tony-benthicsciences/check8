2bc082f40af0fee967036f41ab8f1a4d52164fde
Tony Hedge <tony@benthicsciences.co.uk>
2026-01-02 13:06:22 +0000

    Adds example code, tests.
    Adds example code and unit test to demonstrate use of Check8 trait as a parameter. `calculate_from_string_with_type_as_parameter` function.

--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,30 @@ pub use crate::check8xor::Check8Xor;
 ///     - **Parameter**: string - A string whose byte representation is processed.
 ///     - **Returns**: The final accumulated value as an u8.
 ///
+/// # Examples
+///
+/// Demonstrates use of the Check8 trait as a parameter to a function.
+/// ```
+///use crate::check8::{Check8, Check8Sum, Check8Xor};
+///
+/// fn calculate_from_string_with_type_as_parameter(string: &str, checksum_type: &mut impl Check8) -> u8 {
+///     checksum_type.calculate_from_string(string)
+/// }
+///
+/// fn main()  {
+///     let test_string = "hello";
+///
+///     let mut sum_add = Check8Sum::new();
+///     let result_add = calculate_from_string_with_type_as_parameter(test_string, &mut sum_add);
+///     println!("{}, 8-bit Arithmetic Checksum: {:#04x}", test_string, result_add);
+///
+///     let mut sum_xor = Check8Xor::new();
+///     let result_xor = calculate_from_string_with_type_as_parameter(test_string, &mut sum_xor);
+///     println!("{}, 8-bit XOR Checksum: {:#04x}", test_string, result_xor);
+///     assert!(result_add != result_xor);
+/// }
+///```
+///
 
 pub trait Check8 {
     fn new() -> impl Check8;
@@ -75,3 +99,51 @@ pub trait Check8 {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// ```rust
+    /// Calculates a checksum value from a given string using the provided checksum type.
+    ///
+    /// # Parameters
+    /// - `string`: A reference to the input string for which the checksum will be calculated.
+    /// - `checksum_type`: A mutable reference to an instance of a type that implements the `Check8` trait.
+    ///   This type is used to perform the actual checksum computation.
+    ///
+    /// # Returns
+    /// - A `u8` value representing the computed checksum.
+    ///
+    /// # Notes
+    /// - The function delegates the actual checksum computation to the `calculate_from_string` method
+    ///   of the provided `checksum_type` instance.
+    /// - The `checksum_type` must implement the `Check8` trait, which defines the required behavior
+    ///   for checksum calculation.
+    ///
+    /// ```
+    fn calculate_from_string_with_type_as_parameter(string: &str, checksum_type: &mut impl Check8) -> u8 {
+        checksum_type.calculate_from_string(string)
+    }
+    #[test]
+    fn test_calculate_from_string_with_type_as_parameter() {
+        let test_string = "hello";
+
+        // calculate the 8-bit arithmetic sum and 8-bit xor of the test string
+        let mut expected_sum:u32 = 0;
+        let mut expected_xor:u8 = 0;
+        for val in test_string.as_bytes() {
+            expected_sum += *val as u32;
+            expected_xor ^= *val;
+        }
+        expected_sum %= 256;
+
+        let mut sum_add = Check8Sum::new();
+        let result_add = calculate_from_string_with_type_as_parameter(test_string, &mut sum_add);
+        assert_eq!(result_add, expected_sum as u8);
+
+        let mut sum_xor = Check8Xor::new();
+        let result_xor = calculate_from_string_with_type_as_parameter(test_string, &mut sum_xor);
+        assert_eq!(result_xor, expected_xor);
+    }
+    
+}
